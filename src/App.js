ebac68939bffad9587eae7f7dd5755038d1fa8c9
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-28 10:03:20 +0100

    added new file with navbar and more

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import react from 'react'
 import Home from './component/Home.jsx'
+import Navbar from './component/Navbar.jsx'
 function App() {
-  return <Home />
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  )
 }
 
 export default App
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <div>
+        <ul>
+          <li>
+            <p>HOME</p>
+          </li>
+          <li>
+            <p>ABOUT</p>
+          </li>
+          <li>
+            <p>PRICING</p>
+          </li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
